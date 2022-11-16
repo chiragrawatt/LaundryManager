ed24174a19916c267eea8e08fcbaf0da6e20ab71
Chirag Rawat <chirag.rawat83@gmail.com>
2022-11-16 12:58:21 +0530

    Set up navigation

--- app/src/main/java/com/example/laundrymanager/Fragments/WelcomePage.kt
+++ app/src/main/java/com/example/laundrymanager/Fragments/WelcomePage.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.laundrymanager.R
+import com.example.laundrymanager.databinding.FragmentWelcomePageBinding
 
 class WelcomePage : Fragment() {
+
+    private lateinit var binding: FragmentWelcomePageBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome_page, container, false)
+        binding = FragmentWelcomePageBinding.inflate(layoutInflater)
+
+        binding.btnSignIn.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_welcomePage_to_signIn3)
+        }
+
+        binding.btnSignUp.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_welcomePage_to_signUp2)
+        }
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/laundrymanager/MainActivity.kt
+++ app/src/main/java/com/example/laundrymanager/MainActivity.kt
@@ -10,21 +10,12 @@ import com.example.laundrymanager.databinding.ActivityMainBinding
 class MainActivity() : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var sessionViewModel: SessionViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        sessionViewModel = ViewModelProvider(this).get(SessionViewModel::class.java)
 
-        sessionViewModel.currentUser.observe(this, {user ->
-            binding.txtOutput.text = user
-        })
-
-        binding.btnAction.setOnClickListener {
-            sessionViewModel.setNewUser(binding.edtInput.text.toString())
-        }
     }
 }
